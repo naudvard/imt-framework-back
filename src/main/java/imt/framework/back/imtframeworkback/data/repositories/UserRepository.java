8700b21f47740372adcd6945448dff210ed7c42e
naudvard <nathan.audvard@adeo.com>
2023-10-14 23:01:14 +0200

    stop dev, hibernate search not working properly

--- src/main/java/imt/framework/back/imtframeworkback/data/repositories/UserRepository.java
+++ src/main/java/imt/framework/back/imtframeworkback/data/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package imt.framework.back.imtframeworkback.data.repositories;
 
 import imt.framework.back.imtframeworkback.data.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
     Optional<UserModel> findByMail(String mail);
 }
--- src/main/java/imt/framework/back/imtframeworkback/data/repositories/impl/DishRepositoryExtensionImpl.java
+++ src/main/java/imt/framework/back/imtframeworkback/data/repositories/impl/DishRepositoryExtensionImpl.java
@@ -2,6 +2,7 @@ package imt.framework.back.imtframeworkback.data.repositories.impl;
 
 import imt.framework.back.imtframeworkback.data.models.DishModel;
 import imt.framework.back.imtframeworkback.data.repositories.DishRepositoryExtension;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.search.mapper.orm.Search;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,13 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class DishRepositoryExtensionImpl implements DishRepositoryExtension {
+    @PersistenceContext
     private final EntityManager entityManager;
 
     @Override
     public List<DishModel> search(String terms, int limit, int offset) {
-        return Search.session(entityManager).search(DishModel.class)
+        return Search.
+                session(entityManager).search(DishModel.class)
                 .where(f -> f.match()
                         .fields("title", "description")
                         .matching(terms))
