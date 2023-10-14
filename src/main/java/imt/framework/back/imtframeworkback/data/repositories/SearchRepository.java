ca71ca3dab7db7797baaa702656efef694c8f1c6
Brice Redon <briceredon35@yahoo.fr>
2023-10-14 16:16:16 +0200

    added search repo

---
+++ src/main/java/imt/framework/back/imtframeworkback/data/repositories/SearchRepository.java
@@ -0,0 +1,11 @@
+package imt.framework.back.imtframeworkback.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface SearchRepository<T, ID extends Serializable> extends JpaRepository<T, ID>{
+
+}
---
+++ src/main/java/imt/framework/back/imtframeworkback/data/repositories/impl/SearchRepositoryImpl.java
@@ -0,0 +1,23 @@
+package imt.framework.back.imtframeworkback.data.repositories.impl;
+
+import imt.framework.back.imtframeworkback.data.repositories.SearchRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.io.Serializable;
+
+public class SearchRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements SearchRepository<T, ID> {
+
+    private final EntityManager entityManager;
+
+    public SearchRepositoryImpl (JpaEntityInformation<T, ID> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    public SearchRepositoryImpl (Class<T> domainClass, EntityManager em, EntityManager entityManager) {
+        super(domainClass, em);
+        this.entityManager = entityManager;
+    }
+}
